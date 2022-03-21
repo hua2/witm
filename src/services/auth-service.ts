c849762ce81fd97952319ff636d8c444894e7db9
liuyonghua <liuyonghua1995@gmail.com>
2022-03-21 10:54:54 +0800

    add user-type

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import http from "@/http-common";
+import { UserState } from "@/store/types";
 class AuthService {
   authenticate(data: any): Promise<any> {
     return http.post("api/v1/authenticate", data);
   }
-  account(): Promise<any> {
+  account(): Promise<UserState> {
     return http.get("api/v1/account");
   }
 }
